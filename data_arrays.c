347fa40bd5f6e75d6c65204ccf547e586920678a
Lochana Ranatunga <161409757+lochanalmr@users.noreply.github.com>
2026-07-30 23:55:54 +0530

    Implement arrays required for the program

---
+++ data_arrays.c
@@ -0,0 +1,53 @@
+// Doctor specialties data
+const char *specialtyNames[] = {
+    "General Practice (OPD)",
+    "Pediatrics",
+    "Cardiology",
+    "Neurology"
+};
+
+const int baseConsultationFees[] = {
+    1500,
+    2500,
+    4500,
+    5000
+};
+
+const int consultationTimesPerPatient[] = {
+    15,
+    20,
+    30,
+    30
+};
+
+const int dailyPatientCaps[] = {
+    30,
+    20,
+    12,
+    10
+};
+
+// Hospital ward data
+const char *wardNames[] = {
+    "General Ward",
+    "Pediatric Ward",
+    "Surgical Ward",
+    "ICU (Intensive Care Unit)"
+};
+
+const int dailyBedRates[] = {
+    3000,
+    6000,
+    12000,
+    25000
+};
+
+const int totalBedCapacities[] = {
+    20,
+    10,
+    10,
+    5
+};
+
+// Bed status tracking array
+int bedOccupancy[4][20] = {0};
