b2c2830f4d00a83d509e5b3d9b12209f95b0ff8c
Lochana Ranatunga <161409757+lochanalmr@users.noreply.github.com>
2026-08-24 08:45:57 +0530

    Create header file for calculations.c

---
+++ calculations.h
@@ -0,0 +1,6 @@
+#ifndef CALCULATIONS_H_INCLUDED
+#define CALCULATIONS_H_INCLUDED
+
+int estimatedWaitingTimeCalculator(int patientCount, int specialtyIDs[], int wardIDs[], int countPerSpecialty[]);
+
+#endif // CALCULATIONS_H_INCLUDED
