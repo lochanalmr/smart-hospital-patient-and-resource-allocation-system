5949d3c0270dbf4cfc33d51261e560bfd8e9a022
Lochana Ranatunga <161409757+lochanalmr@users.noreply.github.com>
2026-08-24 08:48:42 +0530

    Create header file for patient_registration.c

---
+++ patient_registration.h
@@ -0,0 +1,6 @@
+#ifndef PATIENT_REGISTRATION_H_INCLUDED
+#define PATIENT_REGISTRATION_H_INCLUDED
+
+void patient_registration(char name[], int *age, int *triageLevel, int *specialtyID, int *admitted, int *wardID, int *daysAdmitted, int countPerSpecialty[], int patientCount, int specialtyIDs[], int wardIDs[]);
+
+#endif // PATIENT_REGISTRATION_H_INCLUDED
