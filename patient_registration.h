d41aee1ab600081bb0022b9f81ad64f8c961be83
lochanalmr <161409757+lochanalmr@users.noreply.github.com>
2026-08-25 17:10:40 +0530

    Implement age subsidy discount calculation

--- patient_registration.h
+++ patient_registration.h
@@ -1,6 +1,6 @@
 #ifndef PATIENT_REGISTRATION_H_INCLUDED
 #define PATIENT_REGISTRATION_H_INCLUDED
 
-int patient_registration(char name[], int *age, int *triageLevel, int *specialtyID, int *admitted, int *wardID, int *daysAdmitted, int countPerSpecialty[], int patientCount, int specialtyIDs[], int wardIDs[], int *waitTime, float *surcharge, int countPerWardID[], int *wardStayCost, float *totalBillValue);
+int patient_registration(char name[], int *age, int *triageLevel, int *specialtyID, int *admitted, int *wardID, int *daysAdmitted, int countPerSpecialty[], int patientCount, int specialtyIDs[], int wardIDs[], int *waitTime, float *surcharge, int countPerWardID[], int *wardStayCost, float *totalBillValue, float *ageSubsidyDiscount);
 
 #endif // PATIENT_REGISTRATION_H_INCLUDED
